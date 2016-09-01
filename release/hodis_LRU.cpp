ab227c2371c8608d896d2e88410ac9140933c4e4
wwh <578867817@qq.com>
2016-09-01 17:04:41 +0800

    LRU cache

---
+++ release/hodis_LRU.cpp
@@ -0,0 +1,67 @@
+/*======================================================
+    > File Name: hodis_LRU.cpp
+    > Author: wwh
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年09月01日 星期四 15时52分05秒
+ =======================================================*/
+
+#include "hodis_LRU.h"
+
+namespace hodis {
+
+LRUCache::LRUCache(uint64_t size) {
+    lruCache = std::make_shared<std::list<Item>>();
+    hashMap  = std::make_shared<std::unordered_map<std::string, Iter>>();
+    lruCacheCurSize = 0;
+    lruCacheMaxSize = size;
+}
+
+LRUCache::~LRUCache() {
+
+}
+
+void
+LRUCache::set(const std::string &key, const std::string &value){
+    std::mutex mutex;
+    auto item = std::make_pair(key, value);
+    
+    std::lock_guard<std::mutex> lock(mutex);
+    if (lruCacheCurSize < lruCacheMaxSize) {
+        lruCacheCurSize++;
+        lruCache->insert(lruCache->begin(), item);
+        hashMap->insert({key, lruCache->begin()});
+    }else {
+        /* erase last item */
+        lruCache->erase(--lruCache->end());
+        lruCache->insert(lruCache->begin(), item);
+        if (hashMap->find(key) != hashMap->end()) {
+            (*hashMap)[key] = lruCache->begin();
+        }else {
+            hashMap->insert({key, lruCache->begin()});
+        }
+    }
+}
+
+LRUCache::Iter 
+LRUCache::get(const std::string &key) {
+    std::mutex mutex;
+    std::lock_guard<std::mutex> lock(mutex);
+    if (hashMap->find(key) != hashMap->end()) {
+        auto iter = (*hashMap)[key];      
+        auto item = *iter;
+        
+        /* 注意：删除节点时，当前迭代器会失效
+         * 其余迭代器不变 */
+        lruCache->erase(iter);
+        lruCache->insert(lruCache->begin(), std::move(item));
+        (*hashMap)[key] = lruCache->begin();
+
+        return (*hashMap)[key];   
+    }else {
+        return lruCache->end();
+    }
+}
+
+
+} /* hodis */
---
+++ release/hodis_LRU.h
@@ -0,0 +1,47 @@
+/*======================================================
+    > File Name: hodis_LRU.h
+    > Author: wwh
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年09月01日 星期四 15时20分44秒
+ =======================================================*/
+
+#include <iostream>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <string>
+#include <mutex>
+#include <utility>
+
+namespace hodis {
+
+class LRUCache {
+    public:
+        using Item = std::pair<std::string, std::string>;
+        using Iter = std::list<Item>::iterator;
+        using LRUCacheList = std::shared_ptr<std::list<Item>>;
+        using LRUHashMap   = std::shared_ptr<std::unordered_map<std::string, Iter>>;
+
+        LRUCache();
+        LRUCache(uint64_t size);
+        ~LRUCache();
+
+    public:
+        void set(const std::string &key, const std::string &value);       
+        Iter get(const std::string &key);
+
+    private:
+        /* LRUCache List */
+        LRUCacheList lruCache;
+        /* hashmap 
+         * key:string 
+         * value:item* 
+         * */
+        LRUHashMap hashMap;
+        uint64_t lruCacheCurSize;
+        uint64_t lruCacheMaxSize;
+};
+
+} /* hodis */
